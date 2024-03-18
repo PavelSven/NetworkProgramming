bccb7786c458959bca6d4bc59304c9dc1431c40b
paveltretyakov94@gmail.com <paveltretyakov94@gmail.com>
2024-03-18 11:49:17 +0400

    Инвертировали стрелки в Spin control. При изменении префикса меняется маска. Определение адреса сети и широковещательного адреса.

--- IPcalcWin32/IPcalcWin32/main.cpp
+++ IPcalcWin32/IPcalcWin32/main.cpp
@@ -5,6 +5,9 @@
 #include "resource.h"
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+VOID GetInfo(HWND hwnd);
+VOID GetIPBytes(DWORD dwIPaddress, CHAR* szIPaddress);
+
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInts, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -17,9 +20,11 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_INITDIALOG:
 	{
-
 		HWND hIPaddress = GetDlgItem(hwnd, IDC_IPADDRESS);
 		SetFocus(hIPaddress);
+
+		HWND hSpinPrefix = GetDlgItem(hwnd, IDC_SPIN_PREFIX);
+		SendMessage(hSpinPrefix, UDM_SETRANGE, 0, MAKELPARAM(32, 0));	//UDM - Up/Down Message
 	}
 		break;
 		
@@ -49,7 +54,6 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		{
 		case IDC_IPADDRESS:
 		{
-
 			DWORD dwIPaddress = 0;
 			SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
 			DWORD first = FIRST_IPADDRESS(dwIPaddress);
@@ -62,13 +66,13 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			CHAR info[256]{};
 			sprintf(info, "%i", first);
 			SendMessage(hStaticInfo, WM_SETTEXT, 0, (LPARAM)info);
-		}
-			
+		}			
 
 		case IDC_IPMASK:
 		{
 			DWORD dwIPmask = 0;
 			SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
+			if (dwIPmask == 0) break;
 			int i = 32;
 			for (; dwIPmask & 0x01 ^ 1; i--) dwIPmask >>= 1;
 			CHAR prefix[5];
@@ -84,30 +88,44 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
-		case IDC_IPADDRESS:
-
+		case IDC_EDIT_PREFIX:
+		{
+			if (HIWORD(wParam) == EN_UPDATE)
+			{
+				HWND hEditPrefix = GetDlgItem(hwnd, IDC_EDIT_PREFIX);
+				CONST INT SIZE = 5;
+				CHAR sz_prefix[SIZE] = {};
+				SendMessage(hEditPrefix, WM_GETTEXT, SIZE, (LPARAM)sz_prefix);
+				DWORD dwPrefix = atoi(sz_prefix);
+				DWORD dwIPmask = UINT_MAX;
+				dwIPmask >>= (32 - dwPrefix);
+				dwIPmask <<= (32 - dwPrefix);
+				SendMessage(GetDlgItem(hwnd, IDC_IPMASK), IPM_SETADDRESS, 0, (LPARAM)dwIPmask);
+				GetInfo(hwnd);
+			}
+		}
 			break;
 
 		case IDOK: 
 		{
-			HWND hIPaddress = GetDlgItem(hwnd, IDC_IPADDRESS);
-			HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
-			HWND hStaticInfo = GetDlgItem(hwnd, IDC_STATIC_INFO);
-			//if ((HWND)wParam == hIPaddress)
-			{
-				DWORD dwIPaddress = 0;
-				SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
-				DWORD first = FIRST_IPADDRESS(dwIPaddress);
-				DWORD dwMask = 0;
-				if (first > 0 && first < 128) dwMask = 0xFF000000;
-				else if (first >= 128 && first < 192) dwMask = 0xFFFF0000;
-				else if (first >= 192 && first < 224) dwMask = 0xFFFFFF00;
-				SendMessage(hIPmask, IPM_SETADDRESS, 0, (LPARAM)dwMask);
+			//HWND hIPaddress = GetDlgItem(hwnd, IDC_IPADDRESS);
+			//HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
+			//HWND hStaticInfo = GetDlgItem(hwnd, IDC_STATIC_INFO);
+			////if ((HWND)wParam == hIPaddress)
+			//{
+			//	DWORD dwIPaddress = 0;
+			//	SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
+			//	DWORD first = FIRST_IPADDRESS(dwIPaddress);
+			//	DWORD dwMask = 0;
+			//	if (first > 0 && first < 128) dwMask = 0xFF000000;
+			//	else if (first >= 128 && first < 192) dwMask = 0xFFFF0000;
+			//	else if (first >= 192 && first < 224) dwMask = 0xFFFFFF00;
+			//	SendMessage(hIPmask, IPM_SETADDRESS, 0, (LPARAM)dwMask);
 
-				CHAR info[256]{};
-				sprintf(info, "%i", first);
-				SendMessage(hStaticInfo, WM_SETTEXT, 0, (LPARAM)info);
-			}
+			//	CHAR info[256]{};
+			//	sprintf(info, "%i", first);
+			//	SendMessage(hStaticInfo, WM_SETTEXT, 0, (LPARAM)info);
+			//}
 		}
 			break;
 
@@ -119,4 +137,54 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	}
 
 	return FALSE;
+}
+
+VOID GetInfo(HWND hwnd)
+{
+	HWND hIPaddress = GetDlgItem(hwnd, IDC_IPADDRESS);
+	HWND hIPmask = GetDlgItem(hwnd, IDC_IPMASK);
+	HWND hStaticInfo = GetDlgItem(hwnd, IDC_STATIC_INFO);
+	
+	DWORD dwIPaddress = 0;
+	DWORD dwIPmask = 0;
+	SendMessage(hIPaddress, IPM_GETADDRESS, 0, (LPARAM)&dwIPaddress);
+	SendMessage(hIPmask, IPM_GETADDRESS, 0, (LPARAM)&dwIPmask);
+
+	CONST INT SIZE = 256;
+	CHAR szAllInfo[SIZE]{};
+	CHAR szInfo[SIZE]{};
+
+	DWORD dwNET_IPaddress = dwIPaddress & dwIPmask;
+	CHAR szNET_IPaddress[17]{};
+	GetIPBytes(dwNET_IPaddress, szNET_IPaddress);
+	sprintf(szInfo, "Network address:\t%s\n", szNET_IPaddress);
+	strcat(szAllInfo, szInfo);
+
+	DWORD dwBroadcast = dwIPaddress | ~dwIPmask;
+	CHAR szBroadcastAddress[17]{};
+	GetIPBytes(dwBroadcast, szBroadcastAddress);
+	sprintf(szInfo, "Broadcast address:\t%s\n", szBroadcastAddress);
+	strcat(szAllInfo, szInfo);
+
+	SendMessage(hStaticInfo, WM_SETTEXT, 0, (LPARAM)szAllInfo);
+}
+
+VOID GetIPBytes(DWORD dwIPaddress, CHAR* szIPaddress)
+{
+	UCHAR bytes[5] = {};
+	for (int i = 4 - 1; i >= 0; i--)
+	{
+		bytes[i] = dwIPaddress & 0xFF;
+		dwIPaddress >>= 8;
+	}
+
+	//CHAR string[16] = {};
+	for (int i = 0; i < 4; i++)
+	{
+		CHAR byte[4];
+		sprintf(byte, "%i", bytes[i]);
+		strcat(szIPaddress, byte);
+		strcat(szIPaddress, ".");
+	}
+	*strrchr(szIPaddress, '.') = 0;
 }
---
+++ IPcalcWin32/IPcalcWinForms/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace IPcalcWinForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //maskedTextBox1.Mask = ###.###.###.###;
+            maskedTextBox1.ValidatingType = typeof(System.Net.IPAddress);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            IPAddress address;
+            string strAddress = maskedTextBox1.Text.Replace(',', '.').Replace(' ', '0');
+            bool parse = IPAddress.TryParse(strAddress, out address);
+            MessageBox.Show(address.ToString());
+        }
+    }
+}
